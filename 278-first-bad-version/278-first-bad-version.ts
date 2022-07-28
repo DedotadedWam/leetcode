df4ab381840fbe90ff91751b2cb7500c7faf6959
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2022-07-28 18:20:21 -0500

    Time: 75 ms (79.35%), Space: 43.1 MB (32.32%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.ts
+++ 278-first-bad-version/278-first-bad-version.ts
@@ -7,9 +7,19 @@
 
 var solution = function(isBadVersion: any) {
     return function(n: number): number {
-        while(isBadVersion(n) && n !== 0) {
-            n--;
-        };
-        return n + 1;
+        if (n === 1) return 1;
+        let lower: number = 1;
+        let upper: number = n;
+        let mid = Math.ceil(n / 2);
+        while(lower + 1 !== upper) {
+            if(isBadVersion(mid)) {
+                upper = mid;
+                mid = Math.floor((upper + lower) / 2)
+            } else {
+                lower = mid;
+                mid = Math.floor((upper + lower) / 2)
+            }
+        }
+        return isBadVersion(lower) ? lower : upper;
     };
 };
