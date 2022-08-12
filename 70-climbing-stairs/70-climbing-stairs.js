f0ea2e2ffcebb6a2a67c468a65920d9a1cf7b996
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2022-08-12 18:13:03 -0500

    Time: 63 ms (90.91%), Space: 41.9 MB (43.59%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -8,7 +8,6 @@ var climbStairs = function(n, memo = {}) {
         return n;
     }
     
-    
     if (memo[n]) return memo[n];
     
     memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
