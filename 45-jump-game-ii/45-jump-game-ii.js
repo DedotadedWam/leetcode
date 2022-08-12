27b04cd955b302de9609d75cd0eb6f8b4871ce29
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2022-08-12 15:20:21 -0500

    Time: 84 ms (80.52%), Space: 45.3 MB (19.29%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let jumps = 0, end = nums.length - 1;
+    
+    function innerFunc(end) {
+        if(end === 0) return;
+        for(let i = 0; i < end; i++) {
+            if (canReachEnd(i, nums[i], end)) {
+                end = i;
+                jumps++;
+                return innerFunc(end);
+            }
+        }
+    }
+    
+    innerFunc(end);
+    return jumps;
+};
+
+function canReachEnd (crtPos, maxJump, end) {
+    return crtPos + maxJump >= end;
+}
